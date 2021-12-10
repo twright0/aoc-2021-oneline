b46f3e50d558d4fc352ab25015d904f2835aabbb
Thomas Wright <twright@twright0.net>
2021-12-09 23:09:00 -0800

    Reduce bad asymptotic memory usage

--- 10b-oneline.py
+++ 10b-oneline.py
@@ -5,7 +5,7 @@ data = lines(get_data(year=2021,day=10))
 
 print(
     (incompletes := list(filter(lambda x:x, list(map(lambda row: (
-        ((scores := {'(': 1, '[': 2, '{': 3, '<': 4}) or True) and
+        (scores := {'(': 1, '[': 2, '{': 3, '<': 4}) and
         (row_state := [row]) and
         ((reduced := list(map(lambda r:
                  ((removable_char_idx := (
@@ -14,7 +14,9 @@ print(
                       (r.find('{}')+1) or
                       (r.find('<>')+1) or 0)-1
                  )) or True) and
-                 ((row_state.append(r[:removable_char_idx]+r[removable_char_idx+2:]) and None) if removable_char_idx >=0 else r),
+                 ((row_state.pop() and row_state.append("") or row_state.append(r[:removable_char_idx]+r[removable_char_idx+2:]) and None)
+                  if removable_char_idx >=0
+                  else r),
                  row_state))[-1]
           ) or True) and
         (0 if any(c not in scores for c in reduced) else reduce(lambda total, c: (5 * total) + scores[c], reduced[::-1], 0))
